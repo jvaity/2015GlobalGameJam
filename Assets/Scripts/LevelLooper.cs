449065273845a66a7c41d34564534077990fde6c
Jason Vaitilingon <jason.vaitilingon@gmail.com>
2015-01-23 22:36:54 +0000

    Level  looper update with static reference to level creator

---
+++ Assets/Scripts/LevelLooper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelLooper : MonoBehaviour 
+{
+	private GameObject tilePrefab;
+	private TileType[,] tileStates = new TileType[30,10];
+
+	private Stack<GameObject> tilePool;
+	private int columnCounter;
+
+//	public void DefineNewLevel(TileType[,] newTileStates)
+//	{
+//		tileStates = newTileStates;
+//	}
+
+	public void DefineNewLevel(Texture2D levelMap)
+	{
+		tileStates = LevelCreator.CreateLevel (levelMap);
+	}
+
+	public void GenerateNextColumn()
+	{
+		++columnCounter;
+
+	}
+
+	// Use this for initialization
+	private void Start () 
+	{
+		tilePool = new Stack<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+	
+	}
+}
