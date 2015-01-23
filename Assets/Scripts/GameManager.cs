73b1f5ec406c6ca87864c7f5363a29b1a1092c68
Richard Simpson <rmsimpson91@gmail.com>
2015-01-23 21:41:56 +0000

    Game Manager Start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SceneState
+{
+    Running,
+    Paused,
+    Won
+}
+
+public class GameManager : MonoBehaviour 
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+
+    public SceneState sceneState;
+    public int numCollectablesLeft = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void GetCollectable()
+    {
+        numCollectablesLeft--;
+
+        if (numCollectablesLeft <= 0)
+        {
+            sceneState = SceneState.Won;
+            EndLevel();
+        }
+    }
+
+    public void TogglePause()
+    {
+        switch(sceneState)
+        {
+            case SceneState.Running:
+                sceneState = SceneState.Paused;
+                Time.timeScale = 0.0f;
+                break;
+
+            case SceneState.Paused:
+                sceneState = SceneState.Running;
+                Time.timeScale = 1.0f;
+                break;
+        }
+    }
+
+    private void EndLevel()
+    {
+        //Show Win UI
+    }
+}
