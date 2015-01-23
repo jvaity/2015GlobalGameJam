9bf2f7c04bedf96049d448ff8367117eaa31e172
Jason Vaitilingon <jason.vaitilingon@gmail.com>
2015-01-23 21:25:15 +0000

    Added breakableTile

---
+++ Assets/Scripts/BreakableTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableTile : MonoBehaviour 
+{
+	public bool willThisTileFuckYouUp;
+
+	private void OnTrigger2DExit(Collider2D collider)
+	{
+		if (collider.CompareTag ("Player")) 
+		{
+			this.gameObject.SetActive(false);
+		}
+
+	}
+}
