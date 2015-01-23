153778ca2940db14e3b5b86f48a10b2040669fe7
Matt Norman <darker99999@googlemail.com>
2015-01-23 22:26:36 +0000

    test map

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelCreator : MonoBehaviour {
+public class LevelCreator {
 
     [SerializeField]
     private GameObject tilePrefab;
 
-	private BreakableTile.TileType[][] CreateLevel(Texture2D image)
+	public static BreakableTile.TileType[,] CreateLevel(Texture2D image)
     {
 
-        BreakableTile.TileType[][] tiles = new BreakableTile.TileType[image.height][];
+        BreakableTile.TileType[,] tiles = new BreakableTile.TileType[image.width, image.height];
         for (int y = 0; y < image.height; y++)
         {
-            tiles[y] = new BreakableTile.TileType[image.width];
             for (int x = 0; x < image.width; x++)
             {
                 Color pixel = image.GetPixel(x, y);
                 if (pixel.r == 1)
                 {
-                    tiles[y][x] = BreakableTile.TileType.Death;
+                    tiles[x,y] = BreakableTile.TileType.Death;
                 }
                 else if (pixel.g == 1)
                 {
-                    tiles[y][x] = BreakableTile.TileType.Block;
+                    tiles[x,y] = BreakableTile.TileType.Block;
                 }
                 else if (pixel.b == 1)
                 {
-                    tiles[y][x] = BreakableTile.TileType.Coin;
+                    tiles[x,y] = BreakableTile.TileType.Coin;
                 }
                 else
-                    tiles[y][x] = BreakableTile.TileType.Empty;
+                    tiles[x,y] = BreakableTile.TileType.Empty;
             }
         }
         return tiles;
