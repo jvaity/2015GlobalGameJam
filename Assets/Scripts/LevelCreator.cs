316284047e8c5cf72e7d162672cdd0a7991ce124
Matt Norman <darker99999@googlemail.com>
2015-01-23 21:39:29 +0000

    Level Creator

---
+++ Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCreator : MonoBehaviour {
+
+	private GameObject CreateLevel(Texture2D image)
+    {
+        Transform parent = new GameObject("Level").transform;
+        for (int y = 0; y < image.height; y++)
+        {
+            for (int x = 0; x < image.width; x++)
+            {
+                Color pixel = image.GetPixel(x, j);
+                if (pixel.a == 0)
+                    continue;
+                else if (pixel.r == 1)
+                {
+                    GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                }
+                else if (pixel.g == 1)
+                {
+                    GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                    tile.renderer.enabled = false;
+                }
+            }
+        }
+        return parent.gameObject;
+    }
+}
