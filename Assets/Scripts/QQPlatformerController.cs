a5fc66b08d4ade7f3423b3be37f5514398fa0e58
maverich1991 <rmsimpson91@gmail.com>
2015-01-24 16:29:32 +0000

    Comment out most of my code

--- Assets/Scripts/QQPlatformerController.cs
+++ Assets/Scripts/QQPlatformerController.cs
@@ -1,33 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlatformerController : MonoBehaviour 
+public class QQPlatformerController : MonoBehaviour 
 {
-    private Vector2 currentTile;
+    //private Vector2 currentTile;
     private Vector3 velocity;
 
-    public float acceleration;
-    public float maxSpeed;
-    public float jumpSpeed;
-    public float gravity;
+    public float acceleration   = 0.2f;
+    public float maxSpeed       = 5.0f;
+    public float jumpSpeed      = 10.0f;
+    public float gravity        = 2.0f;
 
     private bool grounded;
     private CircleCollider2D circleCollider;
 
     private QQLevelGenerator levelGenerator;
 
+    private Vector3 MinYPos
+    {
+        get { return transform.position - (Vector3.up * circleCollider.radius); }
+    }
+
+    private Vector3 MaxXPos
+    {
+        get { return transform.position + (Vector3.right * circleCollider.radius); }
+    }
+
 	// Use this for initialization
 	void Start () 
     {
         circleCollider = GetComponent<CircleCollider2D>();
-        levelGenerator = QQGameManager.Instance.GetComponent<QQLevelGenerator>();
+        levelGenerator = QQGameManager.Instance.LevelGenerator;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
+        Move();
 	}
+
+    public void Spawn(Vector3 position)
+    {
+        velocity = Vector3.zero;
+        transform.position = position;
+    }
 
     private void Move()
     {
@@ -68,11 +84,69 @@ public class PlatformerController : MonoBehaviour
 
     private void CheckIfGrounded()
     {
-        
+        TileType tileTypeBelow = levelGenerator.TileTypeAtPosition(MinYPos);
+
+        switch (tileTypeBelow)
+        {
+            case TileType.Empty:
+                grounded = false;
+                break;
+            case TileType.Block:
+                grounded = true;
+                //DeleteOnNextLoop();
+                transform.position = new Vector3(transform.position.x, Mathf.CeilToInt(MinYPos.y) + circleCollider.radius, transform.position.z);
+                break;
+            case TileType.Death:
+                //Die();
+                break;
+            case TileType.Coin:
+                //Collect();
+                break;
+            case TileType.Spawn:
+                break;
+        }
     }
 
     private void CheckCollisionForward()
     {
+        TileType tileTypeForward = levelGenerator.TileTypeAtPosition(MaxXPos);
 
+        switch (tileTypeForward)
+        {
+            case TileType.Block:
+                grounded = true;
+                transform.position = new Vector3(transform.position.x, Mathf.CeilToInt(MinYPos.y) + circleCollider.radius, transform.position.z);
+                break;
+            case TileType.Coin:
+                //Collect();
+                break;
+            case TileType.Spawn:
+                break;
+        }
     }
+
+    //private void EatTile()
+    //{
+    //    //Eat Sprite/Anim
+
+    //    //Remove Block on GameManager
+    //    QQGameManager.Instance.RemoveBlock(true);
+    //}
+
+    //private void Collect()
+    //{
+    //    //Collect Coin on GameManager
+    //    QQGameManager.Instance.PickUpCollectible();
+    //}
+
+    //private void DeleteOnNextLoop()
+    //{
+    //    QQGameManager.Instance.RemoveBlock(false);
+    //}
+
+    //private void Die()
+    //{
+    //    velocity = Vector3.zero;
+    //    QQGameManager.Instance.GameOver();
+    //}
 }
