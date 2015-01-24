a6029d9829c08776e241b35851dd4e275b3d45e0
Jason Vaitilingon <jason.vaitilingon@gmail.com>
2015-01-24 14:04:10 +0000

    Commented out deprecated code

--- Assets/Deprecated/Scripts/GameManager.cs
+++ Assets/Deprecated/Scripts/GameManager.cs
@@ -1,69 +1,69 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public enum SceneState
-{
-    Running,
-    Paused,
-    Won
-}
-
-public class GameManager : MonoBehaviour 
-{
-    private static GameManager instance;
-    public static GameManager Instance
-    {
-        get { return instance; }
-    }
-
-    public SceneState sceneState;
-    public int numCollectablesLeft = 0;
-
-    private int collectablesCollected = 0;
-
-    void Awake()
-    {
-        if (instance == null)
-            instance = this;
-        else
-            Destroy(this);
-    }
-
-    public void GetCollectable()
-    {
-        numCollectablesLeft--;
-
-        if (numCollectablesLeft <= 0)
-        {
-            sceneState = SceneState.Won;
-            EndLevel();
-        }
-    }
-
-    public void TogglePause()
-    {
-        switch(sceneState)
-        {
-            case SceneState.Running:
-                sceneState = SceneState.Paused;
-                Time.timeScale = 0.0f;
-                break;
-
-            case SceneState.Paused:
-                sceneState = SceneState.Running;
-                Time.timeScale = 1.0f;
-                break;
-        }
-    }
-
-    public void collect()
-    {
-        collectablesCollected++;
-        Debug.Log("collectibles: "+collectablesCollected);
-    }
-
-    private void EndLevel()
-    {
-        //Show Win UI
-    }
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public enum SceneState
+//{
+//    Running,
+//    Paused,
+//    Won
+//}
+//
+//public class GameManager : MonoBehaviour 
+//{
+//    private static GameManager instance;
+//    public static GameManager Instance
+//    {
+//        get { return instance; }
+//    }
+//
+//    public SceneState sceneState;
+//    public int numCollectablesLeft = 0;
+//
+//    private int collectablesCollected = 0;
+//
+//    void Awake()
+//    {
+//        if (instance == null)
+//            instance = this;
+//        else
+//            Destroy(this);
+//    }
+//
+//    public void GetCollectable()
+//    {
+//        numCollectablesLeft--;
+//
+//        if (numCollectablesLeft <= 0)
+//        {
+//            sceneState = SceneState.Won;
+//            EndLevel();
+//        }
+//    }
+//
+//    public void TogglePause()
+//    {
+//        switch(sceneState)
+//        {
+//            case SceneState.Running:
+//                sceneState = SceneState.Paused;
+//                Time.timeScale = 0.0f;
+//                break;
+//
+//            case SceneState.Paused:
+//                sceneState = SceneState.Running;
+//                Time.timeScale = 1.0f;
+//                break;
+//        }
+//    }
+//
+//    public void collect()
+//    {
+//        collectablesCollected++;
+//        Debug.Log("collectibles: "+collectablesCollected);
+//    }
+//
+//    private void EndLevel()
+//    {
+//        //Show Win UI
+//    }
+//}
