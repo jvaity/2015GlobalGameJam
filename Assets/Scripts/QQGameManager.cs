3212f20b9552a19c3017747ecb4e907ba9cdce9a
Matt Norman <darker99999@googlemail.com>
2015-01-24 14:12:13 +0000

    initial GameManager structure

--- Assets/Scripts/QQGameManager.cs
+++ Assets/Scripts/QQGameManager.cs
@@ -3,13 +3,68 @@ using System.Collections;
 
 public class QQGameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public enum GameState
+    {
+        Menu,
+        Game,
+        Win,
+        Lose,
+        Credits
+    }
+
+    private static QQGameManager instance;
+    public static QQGameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private GameState currentState;
+
+    #region Accessors
+
+    public GameState CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+        set
+        {
+            currentState = value;
+        }
+    }
+
+    #endregion
+
+    private void Update()
+    {
+        // Use this to call code from other classes
+        switch (currentState)
+        {
+            case GameState.Menu:
+                break;
+            case GameState.Game:
+                break;
+            case GameState.Win:
+                break;
+            case GameState.Lose:
+                break;
+            case GameState.Credits:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(this);
+        instance = this;
+
+        // Use this to Init any classes
+    }
 }
