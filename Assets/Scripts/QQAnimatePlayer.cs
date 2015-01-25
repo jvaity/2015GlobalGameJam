39e3d21bd0a7880ba57f92d7bb70e2221162183e
Jason Vaitilingon <jason.vaitilingon@gmail.com>
2015-01-25 12:17:23 +0000

    Added animation to player

---
+++ Assets/Scripts/QQAnimatePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QQAnimatePlayer : MonoBehaviour 
+{
+	public SpriteRenderer spriteRenderer;
+	public Sprite sprite1, sprite2;
+	public float waitTime;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		while (true) 
+		{
+			if (spriteRenderer.sprite == sprite1)
+				spriteRenderer.sprite = sprite2;
+			else
+				spriteRenderer.sprite = sprite1;
+				
+			yield return new WaitForSeconds(waitTime);
+		}
+	}
+}
